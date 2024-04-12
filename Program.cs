5962113507c1c235dd8c8dc09fd740caf5275c6f
LAV <vipaleksei007@gmail.com>
2024-04-12 14:33:34 +0300

    new task

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace SimpleToDoList
 
         public static void Main(string[] args)
         {
-            
+            List<Task> task = new List<Task>();
             Console.WriteLine("Привет вот мой консольный вариант ToDo листа выбери задачу:");
             int mesTsk = 0;
 
@@ -15,75 +15,26 @@ namespace SimpleToDoList
                 "2 Создать новую задачу\n" +
                 "3 Удалить существующую задачу\n" +
                 "4 Обновить существующую задачу\n" +
-                "5 Выход  ");
-                
-            List<string> list = new List<string>();
-            ToDo toDo = new ToDo();
-
-            int id = -1;
-            string title = "";
-            string description = "";
-            DateTime dueDate;
-            bool isCopleted =false;
-
-
-            string id_str = "";
-
-
+                "5 Выход  ");    
+            
             do
             {
+
                 string mes = Console.ReadLine();
+               
                 switch (mes)
                 {
                     case "1":
-                        mesTsk = 1;
-                        for (int i = 0; i < 10; i++)
-                        {
-                            Console.WriteLine("");
-                        }
-
+                        Task().ListTask(task);
                         break;
                     case "2":
-                        mesTsk = 2;
-                        
-                        Random r =   new Random();
-                        id = r.Next() + r.Next() / r.Next();
-                        id_str = id.ToString();
-                        Console.WriteLine("Создание новой задачи: \n Заголовок:");
-                        title = Console.ReadLine();
-
-                        Console.WriteLine("Описание:");
-                        description = Console.ReadLine();
-
-                        Console.WriteLine("Дата воддить в формате ( 01.01.2000 ):");
-                        dueDate = Convert.ToDateTime(Console.ReadLine());
-
-                        Console.WriteLine("Выполнена или нет (Y или N):");
-                        string text = Console.ReadLine();
-                        int n = 0;
-                        do
-                            if (text == "y" || text == "Y" || text == "у" || text == "У")
-                            {
-                                n = 1;
-                                isCopleted = true;
-                                text = "";
-                            }
-                            else if (text == "n" || text == "N" || text == "Н" || text == "Н")
-                            {
-                                n = 2;
-                                isCopleted = false;
-                                text = "";
-                            }
-                        while (n == 1 || n == 2 );
-
-
-                        //toDo.AddToDo(id);
+                        Task().CrieateTask(task);
                         break;
                     case "3":
-                        mesTsk = 3;
+                        Task().DeleteTask(task);
                         break;
                     case "4":
-                        mesTsk = 4;
+                        Task().UbdateTask(task);
                         break;
                     case "5":
                         mesTsk = 5;
@@ -105,65 +56,63 @@ namespace SimpleToDoList
         }
     }
 
-    class ToDo
+    class Task
     {
-        //
-        public List<object> TaskToDo = new List<object>();
-
         int id;
         string title;
         string description;
         DateTime dueDate;
         bool isCopleted;
 
-        public void ToDoList(int Id, string Title, string Description, DateTime DueDate, bool IsCopleted, List<object> TaskToDo)
+        public void Tasks(int Id, string Title, string Description, DateTime DueDate, bool IsCopleted)
         {
             this.id = Id;
             this.title = Title;
             this.description = Description;
             this.dueDate = DueDate;
             this.isCopleted = IsCopleted;
-
-            //Надо ли?
-            this.TaskToDo = TaskToDo;
         }
 
-        public void AddToDo(List<object> TaskToDo)
+        public void ListTask(List<Task> tasks)
         {
-
-            this.TaskToDo.Add(TaskToDo);
-
-        }
-        public void IdRemoveToDo(List<string> ListToDo, int Id)
-        {
-
-            if (this.id == Id)
+            if (tasks.Count == 0)
             {
-                this.TaskToDo.Remove(this.id);
+                Console.WriteLine("Задач нет");
+                return;
             }
-
-        }
-        public void UpdateToDo(List<string> ListToDo)
-        {
-
-        }
-
-        public void SearchToDo(List<string> ListToDo, string Title)
-        {
-
-            if (this.title == Title)
+            Console.WriteLine("Список задач:");
+            foreach (var t in tasks)
             {
-                Console.WriteLine($"По запросу найден:\n {ListToDo[this.id]} ");
+                Console.WriteLine(t);
             }
-
         }
 
-        public void IdSearchToDo(List<string> ListToDo, int Id)
+        public void CreateTask(List<Task> tasks)
         {
+            Console.WriteLine("Создание новой задачи:");
+            Console.Write("Заголовок: ");
+            int id = tasks.Count + 1;
+            string title = Console.ReadLine();
+            Console.Write("Описание: ");
+            string des = Console.ReadLine();
+            Console.Write("Дата: ");
+            DateTime dt = DateTime.ParseExact(Console.ReadLine(), "dd.MM.yyyy", null);
+            bool cb = false;
 
-            if (this.id == Id)
+            tasks.Add(new Tasks(id, title, des, dt, cb));
+        }
+
+        public void DeleteTask(List<Task> tasks)
+        {
+            if (true)
             {
-                Console.WriteLine($"По запросу найден:\n {ListToDo[this.id]} ");
+
+            }
+        }
+        public void UbdateTask(List<Task> tasks)
+        {
+            if (true)
+            {
 
             }
         }
